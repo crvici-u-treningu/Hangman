fe0da1d97e0f8b540f3b1e4b1d09d852bc45d94f
Miroslav <gavrilov.miroslav@gmail.com>
2020-07-30 18:36:38 +0200

    Added three versions of hangman

---
+++ Program1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program1
+{
+    static void Main_(string[] _)
+    {
+        Hangman.Start();
+    }
+
+    public class Hangman
+    {
+        public static void Start()
+        {
+            new Hangman("HANGMAN");
+        }
+
+        public Hangman(string word)
+        {
+            int lives = 3;
+            HashSet<char> guessed = new HashSet<char>();
+            bool win = false;
+
+            while(lives > 0)
+            {
+                Console.WriteLine($" Lives left: {lives}\n");
+
+                int left = 0;
+                Console.Write(" ");
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (guessed.Contains(word[i]))
+                        Console.Write(word[i] + " ");
+                    else
+                    {
+                        Console.Write("_ ");
+                        left++;
+                    }
+                }
+
+                Console.WriteLine("\n");
+                if (left == 0)
+                {
+                    win = true;
+                    break;
+                }
+
+                Console.Write(" Guess letter: ");
+                string input = Console.ReadLine();
+                char letter = input.ToUpper().First();
+
+                guessed.Add(letter);
+
+                bool hasLetter = false;
+                for(int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == letter)
+                        hasLetter = true;
+                }
+
+                if (!hasLetter)
+                {
+                    Console.WriteLine("\n * WRONG! *\n");
+                    lives--;
+                }
+            }
+
+            if(win)
+                Console.WriteLine("\n Y O U     W I N !!!!!\n");
+            else
+                Console.WriteLine("\n Y O U     L O S E !!!\n");
+
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Program2
+{
+    static void Main_(string[] _)
+    {
+        Hangman.Start();
+    }
+
+    public enum State
+    {
+        Win,
+        Lost,
+        Guessing
+    }
+
+    public class Hangman
+    {
+        public static void Start()
+        {
+            string[] words = File.ReadAllLines("words.txt");
+            var random = new Random();
+
+            string chosenWord = words[random.Next(0, words.Length)].ToUpper();
+            Console.WriteLine(chosenWord);
+            HashSet<char> requiredLetters = new HashSet<char>(chosenWord);
+
+            int lives = 3;
+
+            State state = State.Guessing;
+
+            HashSet<char> guessed = new HashSet<char>();
+
+            while(state == State.Guessing)
+            {
+                if (requiredLetters.Except(guessed).Count() == 0)
+                    state = State.Win;
+
+                if (state == State.Guessing)
+                {
+                    Console.WriteLine($" Lives left: {lives}\n");
+                    Console.Write(" ");
+
+                    for (int i = 0; i < chosenWord.Length; i++)
+                    {
+                        if (guessed.Contains(chosenWord[i]))
+                            Console.Write(chosenWord[i] + " ");
+                        else
+                        {
+                            Console.Write("_ ");
+                        }
+                    }
+
+                    Console.WriteLine("\n");
+
+                    Console.Write(" Guess letter: ");
+                    string input = Console.ReadLine();
+                    char letter = input.ToUpper().First();
+
+                    guessed.Add(letter);
+
+                    if (!requiredLetters.Contains(letter))
+                    {
+                        Console.WriteLine("\n * WRONG! *\n");
+                        lives--;
+
+                        if (lives <= 0)
+                            state = State.Lost;
+                    }
+                }
+            }
+
+            if(state == State.Win)
+                Console.WriteLine("\n Y O U     W I N !!!!!\n");
+            else
+                Console.WriteLine("\n Y O U     L O S E !!!\n");
+
+        }
+    }
+}
